04b27f9c6257d3a7c96e3e8da5e96c673109d302
Nassim Abed <nabed@sodifrance.fr>
2018-11-02 15:13:36 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot Api Rest Jpa
+
+https://www.javacodegeeks.com/2018/08/restful-api-spring-rest-data-jpa-h2.html
---
+++ src/main/java/com/example/SpringRestJpa/SpringRestJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringRestJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestJpaApplication.class, args);
+	}
+}
