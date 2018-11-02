dd2dde7069ac52af66c4511acd5d9017cbc98205
Nassim Abed <nabed@sodifrance.fr>
2018-11-02 16:14:02 +0100

    Create Employee Interface Service

---
+++ src/main/java/com/example/SpringRestJpa/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.SpringRestJpa.service;
+
+import java.util.List;
+
+import com.example.SpringRestJpa.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> retrieveEmployees();
+	
+	public Employee getEmployee(Long employeeId);
+	
+	public void saveEmployee(Employee employee);
+	
+	public void deleteEmployee(Long employeeId);
+	
+	public void updateEmployee(Employee employee);
+}
